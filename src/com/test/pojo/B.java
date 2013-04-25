e155a73723664bc102439a56304d32b725ec7be6
luyuandeng <awaitdeng@163.com>
2013-04-25 15:42:12 +0800

    commit

---
+++ src/com/test/pojo/B.java
@@ -0,0 +1,17 @@
+package com.test.pojo;
+
+public class B {
+	private String password;
+
+	private String age ;
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
